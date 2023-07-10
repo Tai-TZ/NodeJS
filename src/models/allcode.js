39449ad70bd9544e13c7249389761717a8b7b7f9
Thanh Tai <n.t.tai435@gmail.com>
2023-07-11 00:07:20 +0700

    api top doctor, fix bug, communicate table db 10/7/2023

--- src/models/allcode.js
+++ src/models/allcode.js
@@ -10,14 +10,16 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Allcode.hasMany(models.User, { foreignKey: 'positionId', as: 'positionData' })
+      Allcode.hasMany(models.User, { foreignKey: 'gender', as: 'genderData' })
+
     }
   };
-  Allcode.init({ 
-    key: DataTypes.STRING,
+  Allcode.init({
+    keyMap: DataTypes.STRING,
     type: DataTypes.STRING,
-    valueEn: DataTypes.STRING, 
-    valueVi: DataTypes.STRING, 
+    valueEn: DataTypes.STRING,
+    valueVi: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Allcode',
--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController"
-
+import doctorController from "../controllers/doctorController"
 
 
 let router = express.Router();
@@ -36,6 +36,9 @@ let initWebRoutes = (app) => {
     router.delete('/api/delete-user', userController.handleDeleteUser)
     router.get('/api/allcode', userController.getAllCode)
 
+
+    //api doctor
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome)
 
 
     return app.use("/", router)
