b8f2090b7bbabf1558f43fd12c65f434cffb2f11
Thanh Tai <n.t.tai435@gmail.com>
2023-08-11 16:30:37 +0700

    Update models clinic(img,deschtml, descMark), route clinic, api add New Clinic 11/8/2023

--- src/route/web.js
+++ src/route/web.js
@@ -4,6 +4,7 @@ import userController from "../controllers/userController"
 import doctorController from "../controllers/doctorController"
 import patientController from "../controllers/patientController"
 import specialtyController from "../controllers/specialtyController";
+import clinicController from "../controllers/clinicController"
 
 let router = express.Router();
 let initWebRoutes = (app) => {
@@ -59,12 +60,13 @@ let initWebRoutes = (app) => {
 
 
     //api specialty 
-    router.post('/api/create-new-specialty', specialtyController.createSpecialty) // verify bằng token và doctorId trên thanh Url
+    router.post('/api/create-new-specialty', specialtyController.createSpecialty)
     router.get('/api/get-specialty', specialtyController.getAllSpecialty)
     router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById)
 
 
-
+    //api clinic
+    router.post('/api/create-new-clinic', clinicController.createClinic)
 
 
     return app.use("/", router)
